203dabfe31d3dd5b645ebb1c848ce8534e480225
Jupiter 404 <92510876+Jupiter404E@users.noreply.github.com>
2022-11-25 04:11:05 +0200

    Update type.py

--- SpendScheme/type.py
+++ SpendScheme/type.py
@@ -27,26 +27,26 @@ class ColorCode(object):
                 setattr(self, name, code_to_chars(value))
 
 class __Colour(ColorCode):
-    end = 0
-    black = 30
-    red = 31
-    green = 32
+    end    = 0
+    black  = 30
+    red    = 31
+    green  = 32
     yellow = 33
-    blue = 34
+    blue   = 34
     violet = 35
-    cyan = 37
-    white = 37
+    cyan   = 37
+    white  = 37
     
 class __ColourBG(ColorCode):
-    end = 0
-    black = 40
-    red = 41
-    green = 42
+    end    = 0
+    Black  = 40
+    red    = 41
+    green  = 42
     yellow = 43
-    blue = 44
+    blue   = 44
     violet = 45
-    cyan = 47
-    white = 37
+    cyan   = 47
+    white  = 37
 
 class __Style(ColorCode):
     end      = "\33[0m"
