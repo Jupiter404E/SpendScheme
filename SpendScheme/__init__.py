7041a695e8c2a0a40c300179d39462c79420ba87
Jupiter404E <92510876+Jupiter404E@users.noreply.github.com>
2023-02-02 17:53:50 +0200

    Update __init__.py

--- SpendScheme/__init__.py
+++ SpendScheme/__init__.py
@@ -8,7 +8,7 @@ __title__ = "SpendScheme"
 __author__ = "Jupiter404E"
 __license__ = "MIT"
 __copyright__ = "Copyright 2022-present Jupiter404E"
-__version__ = "0.3"
+__version__ = "0.4"
 
 from .formatting import *
 from .textStyle import *
