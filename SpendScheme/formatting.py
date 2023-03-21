5c299f29b849f7ca6b570e84bb480cad88b56e1b
Jupiter404E <92510876+Jupiter404E@users.noreply.github.com>
2023-03-21 11:45:30 +0100

    Update formatting.py

--- SpendScheme/formatting.py
+++ SpendScheme/formatting.py
@@ -1,51 +1,32 @@
 """
 
-    Copyright (C) 2022 Jupiter404E.
+Copyright (C) 2022-2023 Jupiter404E.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2022-2023 present Jupiter404E
+:license: MPL-2.0 license, see LICENSE for more details.
 
 """
 
 class Formatting:
 
     __slots__ = (
-        "text"
+        'text'
     )
 
-    def __init__(
-        self,
-        *,
-        text = None,
-    ):
+    def __init__(self, *, text = None):
         if text is not None:
             self.text = text
 
         elif text is None:
-            self.text = "None"
+            self.text = None
 
-    def lowerTranslit(
-        self
-    ):    
+    def lowerTranslit(self):    
         self.text = str(self.text).lower()
 
-    def autoList(
-        self,
-        list
-    ):
-        index = 0
-
-        while index < len(list):
-            print("{}. ".format(index) + list[index])
-            index = index + 1
-
-    def answer(
-        self,
-        color = None
-    ):
+    def answer(self, color = None):
         if color is not None:
-            return (
-                    color + self.text + "\33[0m"
-            )
+            return (color + self.text + "\33[0m")
         
         else:
-            return (
-                    self.text
-            ) 
+            return (self.text)
