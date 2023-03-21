1f67bd469cb90b225144cab43853d6e0c28ecbe0
Jupiter404E <92510876+Jupiter404E@users.noreply.github.com>
2023-03-21 11:45:04 +0100

    Update __init__.py

--- SpendScheme/__init__.py
+++ SpendScheme/__init__.py
@@ -1,15 +1,31 @@
 """
 
-    Copyright (C) 2022 Jupiter404E.
+Copyright (C) 2022-2023 Jupiter404E.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2022-2023 present Jupiter404E
+:license: MPL-2.0 license, see LICENSE for more details.
 
 """
 
-__title__ = "SpendScheme"
-__author__ = "Jupiter404E"
-__license__ = "MIT"
-__copyright__ = "Copyright 2022-present Jupiter404E"
-__version__ = "0.4"
+__title__ = 'SpendScheme'
+__author__ = 'Jupiter404E'
+__license__ = 'MPL-2.0 license'
+__copyright__ = 'Copyright 2022-2023 present Jupiter404E'
+__version__ = '0.5'
+
+__all__ = [
+    'formatting',
+    'textStyle',
+    'table',
+    'list'
+]
 
 from .formatting import *
-from .textStyle import *
+from .textStyle import (
+    Color,
+    ColorBG,
+    Style
+)
 from .table import *
+from .list import *
